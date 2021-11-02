db5f7b24172dc0aa7515d9d367ed3a3366733cba
charliny <jessicacharliny@gmail.com>
2021-11-02 13:50:39 -0300

    composer and autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6e82a42fd32a2c0602a34daf217b398185bba417',
+        'name' => 'jessica/clean',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jessica/clean' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6e82a42fd32a2c0602a34daf217b398185bba417',
+            'dev_requirement' => false,
+        ),
+    ),
+);
